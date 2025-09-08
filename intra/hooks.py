fcf3cb005bc3df5bb0f381c50ba300c178a2a448
Ubaid-AI <ubaidkhanzada8@gmail.com>
2025-09-08 17:25:33 +0500

    CSS Upadted

--- intra/hooks.py
+++ intra/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/intra/css/intra.css"
+app_include_css = "/assets/intra/css/intra.css"
 # app_include_js = "/assets/intra/js/intra.js"
 
 # include js, css files in header of web template
